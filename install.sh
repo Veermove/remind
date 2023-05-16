13f8b3d38efe5aff8f9d3afd9ffd2b53344aa72c
veermove <75857174+Veermove@users.noreply.github.com>
2023-05-17 01:06:07 +0200

    conditional file removal

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
-rm /home/$USER/.local/bin/remind
+[ -e /home/$USER/.local/bin/remind ] && rm /home/$USER/.local/bin/remind
 ln -s "$(pwd)/src/main.py" /home/$USER/.local/bin/remind
 cp doc/remind.1 /home/$USER/.local/share/man/man1
